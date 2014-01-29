19aa542b594a7569720e54efcc021637f263af3f
RouxRC <b.ooghe@gmail.com>
2014-01-30 00:09:39 +0100

    prepare for automation

--- bin/download_table.sh
+++ bin/download_table.sh
@@ -2,5 +2,5 @@
 
 for (( i = 0 ; i < 110 ; i++ )) do
    nb=$(expr $i '*' 100)
-   wget -O data/tableau/$(printf '%06d' $nb).table "http://www2.assemblee-nationale.fr/reserve_parlementaire/plf/(offset)/$nb/(Legislature)/14/(Budget)/2013/(typeTri)/dest"
+   wget -q -O data/tableau/$(printf '%06d' $nb).table "http://www2.assemblee-nationale.fr/reserve_parlementaire/plf/(offset)/$nb/(Legislature)/14/(Budget)/2013/(typeTri)/dest"
 done
--- bin/generate.sh
+++ bin/generate.sh
@@ -17,4 +17,5 @@ join -a 1 -t ';' -1 6 -2 19 data/final/reserve_sans_nosdeputes.csv  data/deputes
   sed 's/^\(;.*;\(Groupe \([^;]\+\)\);.*\)$/\2\1;;;;;;;;;;;;;;\3;;;;;;;;/' |
   sed "s/^\(;.*;Présidence de l'Assembl.*\)$/Présidence de l'Assemblée (Réserve institutionnelle)\1;;;;;;;;;;;;;;;;;;;;;;/" |
   sort -n -t ';' -k 2,2 >> data/final/reserve.csv
-awk -F ";" 'BEGIN { OFS = ";"} {print $2,$3,$4,$5,$6,$12,$14,$1,$13,$15,$16,$17,$18,$19,$20,21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34}' data/final/reserve.csv > data/final/reserve-assemblee-2013.csv
+awk -F ";" 'BEGIN { OFS = ";"} {print $2,$3,$4,$5,$6,$12,$14,$1,$13,$15,$16,$17,$18,$19,$20,21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34}' data/final/reserve.csv > data/data/final/reserve-assemblee-2013.csv.tmp
+mv data/data/final/reserve-assemblee-2013.csv{.tmp,}
