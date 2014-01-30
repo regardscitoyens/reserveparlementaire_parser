ce98b0b7cfd5e534512e5bd7858f5ed45197e88b
RouxRC <b.ooghe@gmail.com>
2014-01-30 11:59:58 +0100

    oups...

--- bin/download_json.sh
+++ bin/download_json.sh
@@ -6,7 +6,7 @@ for (( i = 1 ; i < 11043 ; i++ )) do
    echo "http://www2.assemblee-nationale.fr/reserve_parlementaire/reserve_parlementaire_detail_json?beneficiaire=$i"
 done > /tmp/json.url
 
-wget -i -q /tmp/json.url
+wget -q -i /tmp/json.url
 
 rm /tmp/json.url
 
