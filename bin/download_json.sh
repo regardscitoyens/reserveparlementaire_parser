996a83500a5b7c536142990a7d528d875bbc2a7f
RouxRC <b.ooghe@gmail.com>
2014-02-02 13:19:05 +0100

    clean json cache to avoid piling successive versions from wget

--- bin/download_json.sh
+++ bin/download_json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd data/json
-
+rm *json*
 for (( i = 1 ; i < 11043 ; i++ )) do
    echo "http://www2.assemblee-nationale.fr/reserve_parlementaire/reserve_parlementaire_detail_json?beneficiaire=$i"
 done > /tmp/json.url
