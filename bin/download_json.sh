9122adeb88c39f3dd5de8d34b1d7bcf79e564eee
RouxRC <b.ooghe@gmail.com>
2014-02-09 11:29:35 +0100

    make sure to download data when AN is slow

--- bin/download_json.sh
+++ bin/download_json.sh
@@ -3,11 +3,23 @@
 cd data/json
 rm *json*
 for (( i = 1 ; i < 11043 ; i++ )) do
-   echo "http://www2.assemblee-nationale.fr/reserve_parlementaire/reserve_parlementaire_detail_json?beneficiaire=$i"
-done > /tmp/json.url
-
-wget -q -i /tmp/json.url
-
-rm /tmp/json.url
+  url="http://www2.assemblee-nationale.fr/reserve_parlementaire/reserve_parlementaire_detail_json?beneficiaire=$i"
+  ct=0
+  while [ $ct -lt 5 ] ; do
+    curl -s -L "$url" > "$i.json"
+    size=$(cat "$i.json" | wc -c)
+    if [ $size -gt 10 ]; then
+      break
+    fi
+    ct=$(($ct+1))
+    if [ $ct -eq 5 ]; then
+      if [ $size -ne 2 ]; then
+        echo "FAILED to download data at $url"
+      fi
+    else
+      sleep 1
+    fi
+  done
+done
 
 cd -
