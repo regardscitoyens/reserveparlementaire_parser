a7b7ae90ab60cbb25d5b43b90d9d1d8a558168fe
Tangui Morlier <tangui@tangui.eu.org>
2015-02-12 23:35:36 +0100

    Intègre les sauts de lignes + corrections des évolutions dans le svg produit par raw

--- bin/title_svg_raw.sh
+++ bin/title_svg_raw.sh
@@ -1,7 +1,8 @@
-grep -v "<text" svg > bulles
+cat $1 | sed 's/\(<\/[^>]*>\)/\1\n/g' | sed 's/><text/>\n<text/' > svg
+grep -v "<text" svg > bulles.svg
 grep "<text " svg | while read line; do
-  coords=$(echo $line | sed 's/^.*translate(//' | sed 's/)">.*$//')
-  text=$(echo $line | sed 's/^.*)">//' | sed 's/<\/text>//')
-  sed 's/\('"$coords"')"[^>]*>\)/\1<title>'"$text"'<\/title>/' bulles > bulles.new
-  mv -f bulles{.new,}
+  coords=$(echo $line | sed 's/^.*translate(//' | sed 's/)".*$//')
+  text=$(echo $line | sed 's/^<text[^>]*>//' | sed 's/<\/text>.*//' | sed 's/, / /')
+  sed 's/\('"$coords"')"[^>]*>\)/\1<title>'"$text"'<\/title>/' bulles.svg > bulles.new
+  mv -f bulles.new bulles.svg
 done
