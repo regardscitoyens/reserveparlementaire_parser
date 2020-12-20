399184657d6d12c9f5452ba80d564760ec214eb4
RouxRC <b.ooghe@gmail.com>
2020-12-21 00:07:56 +0100

    new https routes

--- bin/download_year.sh
+++ bin/download_year.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname $0)/..
 year=$1
-curl -s http://www2.assemblee-nationale.fr/static/budget/plf$year/reserve_parlementaire.csv > data/final/reserve-assemblee-$year.csv
+curl -sL https://www2.assemblee-nationale.fr/static/budget/plf$year/reserve_parlementaire.csv > data/final/reserve-assemblee-$year.csv
